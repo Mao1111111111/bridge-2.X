1357bc5c97f64102c1a593a4ebd0ae119b8d424f
Mao1111111111 <1309142927@qq.com>
2023-09-13 10:57:28 +0800

    .

--- src/screen/Login.js
+++ src/screen/Login.js
@@ -326,7 +326,7 @@ export default function Login() {
                 }
               ]
             }
-          >v2.08</Text>
+          >v2.09</Text>
           <View style={[styles.box]}>
             {/* 判断是否同步，默认不同步 */}
             {/* 这里是登录后，同步数据 */}
