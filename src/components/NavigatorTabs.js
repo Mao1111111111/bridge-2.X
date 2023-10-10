d5281f9c6bf623ce62a98942635881f01c1fc89c
Mao1111111111 <1309142927@qq.com>
2023-10-10 17:27:05 +0800

    ...

--- src/components/NavigatorTabs.js
+++ src/components/NavigatorTabs.js
@@ -240,7 +240,7 @@ const TabBar = ({state, navigation, descriptors, headerItems, pid}) => {
             }
           ]
         }
-      >v3.06</Text>
+      >v3.07</Text>
       {/* 检测公司名称 */}
       {/* <Text
         style={
