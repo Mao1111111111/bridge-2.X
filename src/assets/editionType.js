7add4af12d86ead06bb2f118b86a49d789ed7498
超鱼儿 <1723167847@qq.com>
2023-11-24 12:16:26 +0800

    公共配置

--- src/assets/editionType.js
+++ src/assets/editionType.js
@@ -1,20 +1,17 @@
 /* 
     版本类型
  */
-// 1、检立得--jld
-// 2、客户--Client
-export const editionType = 'jld'
+// 1、简立得
+// 2、黑龙江省工程质量道桥检测中心
+export const editionType = '简立得'
 
 // 类型列表
 export const editionList = {
-    // 检立得
-    jld:{
-        // 
+    '简立得':{
         // 上传到 华为云(OBS) 还是 紫光云(AWS)
         UploadObjectStorageName:'OBS'
     },
-    // 客户版
-    Client:{
+    '黑龙江省工程质量道桥检测中心':{
         // 上传到 华为云(OBS) 还是 紫光云(AWS)
         UploadObjectStorageName:'AWS'
     }
