3d793381b17b92bab9a58cfab04a47681f39bee4
超鱼儿 <1723167847@qq.com>
2024-06-24 09:33:26 +0800

    加入高德定位

--- src/utils/location.js
+++ src/utils/location.js
@@ -1,26 +1,47 @@
 import Geolocation from 'react-native-geolocation-service';
 
+// 高德定位
+import { init,addLocationListener,start, stop,setInterval} from "react-native-amap-geolocation";
+
 let position = {};
+
+// 高德定位
+const initWatchAmapGPS = async () => {
+  await init({
+      android: "a6b8a1e83fd87a7c31f1ae2d0777f479",
+      ios:''
+  });
+  addLocationListener((location) => {
+    position = location
+  });
+  // 开始连续定位
+  start();
+}
+
+// 原生定位
+const initWatchGPS = () => {
+  Geolocation.watchPosition(
+    _position => {
+      console.info(_position);
+      position === _position;
+    },
+    error => {
+      // See error code charts below.
+      console.log(error.code, error.message);
+    },
+    {
+      enableHighAccuracy: false,
+      forceLocationManager: true,
+      forceRequestLocation: true,
+      timeout: 15000,
+      maximumAge: 10000,
+    },
+  );
+}
 
 export default {
   initWatch: () => {
-    Geolocation.watchPosition(
-      _position => {
-        console.info(_position);
-        position === _position;
-      },
-      error => {
-        // See error code charts below.
-        console.log(error.code, error.message);
-      },
-      {
-        enableHighAccuracy: false,
-        forceLocationManager: true,
-        forceRequestLocation: true,
-        timeout: 15000,
-        maximumAge: 10000,
-      },
-    );
+    initWatchGPS()
   },
   getPosition: () => position,
 };
