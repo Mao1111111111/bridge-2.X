974f2c2b99d6290094adf5e8756b0250c345a47e
超鱼儿 <1723167847@qq.com>
2024-07-02 15:00:31 +0800

    Revert "打包用"
    This reverts commit 043968a36ac86fbfbdc921c86af76c94cec1e6e8.

--- src/assets/editionType.js
+++ src/assets/editionType.js
@@ -3,7 +3,7 @@
  */
 // 1、简立得
 // 2、黑龙江省工程质量道桥检测中心
-export const editionType = '简立得'
+export const editionType = '黑龙江省工程质量道桥检测中心'
 
 // 类型列表
 export const editionList = {
@@ -12,5 +12,11 @@ export const editionList = {
         loginPageCompanyName:'欢迎使用 简立得®',
         // 上传到 华为云(OBS)
         UploadObjectStorageName:'OBS'
+    },
+    '黑龙江省工程质量道桥检测中心':{
+        // 登录页公司名
+        loginPageCompanyName:'黑龙江省工程质量道桥检测中心',
+        // 上传到 紫光云(AWS)
+        UploadObjectStorageName:'AWS'
     }
 }
--- src/assets/uploadConfig/OBSConfig.js
+++ src/assets/uploadConfig/OBSConfig.js
@@ -3,18 +3,30 @@
  */
 
 
+// //-----个人测试的 华为云OBS  
+//ak
+// export const ak = 'HIIUVAQURPR6DVKSNMLB'
+// //sk
+// export const sk = 'ZgOc2cZwn24eScjPOUL7achNETDcAHhlqNAeF7pD'
+// //endPoint
+// export const endPoint = 'https://obs.cn-east-3.myhuaweicloud.com'
 
+// //存储 检测数据的桶
+// export const BucketName_storeTestData = 'roadtestapp-obstest'
+
+// // 地址
+// export const OBSHost = 'https://roadtestapp-obstest.obs.cn-east-3.myhuaweicloud.com/'
 
 
 //-----公司的 华为云OBS  
 //ak
-export const ak = ' *************'
+export const ak = 'U9XPE4OB9XJXXQ4ABZJV'
 //sk
-export const sk = ' *************'
+export const sk = 'j4kX0zwcdV7HswiHBEjhl5BXTtormJcKW5vmduEU'
 //endPoint
-export const endPoint = ' *************'
+export const endPoint = 'https://obs.cn-north-4.myhuaweicloud.com'
 
 //存储 检测数据的桶
-export const BucketName_storeTestData = ' *************'
+export const BucketName_storeTestData = 'jld-test-tong'
 
-export const OBSHost = ' *************'
+export const OBSHost = 'https://jld-test-tong.obs.cn-north-4.myhuaweicloud.com/'
