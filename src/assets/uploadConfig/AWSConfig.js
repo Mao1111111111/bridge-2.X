043968a36ac86fbfbdc921c86af76c94cec1e6e8
超鱼儿 <1723167847@qq.com>
2024-07-02 14:51:37 +0800

    打包用

--- src/assets/uploadConfig/AWSConfig.js
+++ src/assets/uploadConfig/AWSConfig.js
@@ -2,12 +2,12 @@
     AWS配置
  */
 export const BaseAWSConfig = {
-    accessKeyId:'IjXECJQB745sth9b',
-    secretAccessKey:'pJwy6mjp1GolNw1yH7Vtrkfx7ktvd4',
-    endpoint:'http://oss-cn-north-2.unicloudsrv.com'
+    accessKeyId:' *************',
+    secretAccessKey:' *************',
+    endpoint:' *************'
 }
 
 export const AWSBucket = {
     //默认桶
-    defaultBucket:'androiddata'
+    defaultBucket:' *************'
 }
--- src/assets/uploadConfig/OBSConfig.js
+++ src/assets/uploadConfig/OBSConfig.js
@@ -3,30 +3,18 @@
  */
 
 
-// //-----个人测试的 华为云OBS  
-//ak
-// export const ak = 'HIIUVAQURPR6DVKSNMLB'
-// //sk
-// export const sk = 'ZgOc2cZwn24eScjPOUL7achNETDcAHhlqNAeF7pD'
-// //endPoint
-// export const endPoint = 'https://obs.cn-east-3.myhuaweicloud.com'
 
-// //存储 检测数据的桶
-// export const BucketName_storeTestData = 'roadtestapp-obstest'
-
-// // 地址
-// export const OBSHost = 'https://roadtestapp-obstest.obs.cn-east-3.myhuaweicloud.com/'
 
 
 //-----公司的 华为云OBS  
 //ak
-export const ak = 'U9XPE4OB9XJXXQ4ABZJV'
+export const ak = ' *************'
 //sk
-export const sk = 'j4kX0zwcdV7HswiHBEjhl5BXTtormJcKW5vmduEU'
+export const sk = ' *************'
 //endPoint
-export const endPoint = 'https://obs.cn-north-4.myhuaweicloud.com'
+export const endPoint = ' *************'
 
 //存储 检测数据的桶
-export const BucketName_storeTestData = 'jld-test-tong'
+export const BucketName_storeTestData = ' *************'
 
-export const OBSHost = 'https://jld-test-tong.obs.cn-north-4.myhuaweicloud.com/'
+export const OBSHost = ' *************'
